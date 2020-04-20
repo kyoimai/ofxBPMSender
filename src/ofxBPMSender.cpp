f6ad15c8b62fdb78e4a25a60e7dd05b74e5c8fd7
kyoimai <37477849+kyoimai@users.noreply.github.com>
2020-04-20 20:47:48 +0900

    second

--- src/ofxBPMSender.cpp
+++ src/ofxBPMSender.cpp
@@ -1,90 +1,73 @@
 //
 //  ofxBPMSender.cpp
-//  bpm_sender
+//  ofxBPMSender
 //
-//  Created by kyo imai on 2020/04/06.
+//  Created by 今井亨 on 2020/04/18.
 //
 
 #include "ofxBPMSender.hpp"
 
-ofEvent<bool> ofxBPMSender::attack = ofEvent<bool>();
+//絶対に書くやつ----------------------------------------
 
-ofxBPMSender::ofxBPMSender(){}
-ofxBPMSender::~ofxBPMSender(){}
-
-
-//--------------------------------------------------------------
-void ofxBPMSender::setButton(ofVec2f position, ofVec2f size){
-    this->position = position;
-    this->size = size;
-    bpm = 0.0;
-    ofRegisterMouseEvents(this); //イベントの許可
-    ofRegisterKeyEvents(this);
-}
-
-void ofxBPMSender::updateBPM(){
+void ofxBPMSender::update(){
     noticeBPM();
 }
 
-void ofxBPMSender::drawButton(){
-    noticeBPM();
-    if(is_attack == true){
-        ofSetColor(255);
-        ofDrawRectangle(position.x-100, position.y, size.x, size.y);
-        ofNoFill();
-        ofSetColor(0, 0, 0);
-        ofDrawRectangle(position.x-100, position.y, size.x, size.y);
-        ofFill();
-    }
-    if(is_attack == false){
-        ofSetColor(0);
-        ofDrawRectangle(position.x-100, position.y, size.x, size.y);
-    }
-    
-    ofSetColor(255, 0, 0);
-    ofDrawRectangle(position.x, position.y, size.x, size.y);
-    ofSetColor(255);
-    ofDrawBitmapString("tap!!!", position.x + 20, position.y + 40);
-    
-    ofSetColor(255);
-}
-
-//--------------------------------------------------------------
 float ofxBPMSender::getBPM(){
     return bpm;
 }
 
-//--------------------------------------------------------------
-//--------------------------------------------------------------
-//--------------------------------------------------------------
-//--------------------------------------------------------------
+bool ofxBPMSender::getAttack(){
+    return isAttack;
+}
 
-void ofxBPMSender::noticeBPM(){
-    if(second_time_data + time_distance < ofGetElapsedTimef()){
-        second_time_data += time_distance;
-        ofNotifyEvent(attack, is_attack);
-        is_attack = true;
-    }else{
-        is_attack = false;
+
+
+//key----------------------------------------------------------------
+void ofxBPMSender::keyReleased(int key){
+    if(tap_count % 2 == 0){
+        ofLogNotice() << "first_tap";
+        first_time = ofGetElapsedTimef();
     }
+    if(tap_count % 2 == 1){
+        ofLogNotice() << "second_tap";
+        second_time = ofGetElapsedTimef();
+        second_time_data = second_time;
+        time_distance = second_time - first_time;
+        calculateBPM(time_distance);
+    }
+    tap_count++;
 }
 
-void ofxBPMSender::calculateBPM(float time_distance){
-    float _bpm = 60.f / time_distance;
-    bpm = _bpm;
+
+//mouse--------------------------------------------------------------
+void ofxBPMSender::setupButton(glm::vec2 _position, float _size){
+    position = _position;
+    size = _size;
 }
 
-//--------------------------------------------------------------
-void ofxBPMSender::mouseReleased(ofMouseEventArgs & args){
-    if(position.x < args.x && args.x < position.x + size.x){
-        if(position.y < args.y && args.y < position.y < args.y){
-            is_on = true;
+void ofxBPMSender::drawButton(){
+    if(isAttack){
+        ofSetColor(255);
+        ofDrawRectangle(position.x, position.y, size, size);
+    }else{
+        ofSetColor(0);
+        ofDrawRectangle(position.x, position.y, size, size);
+    }
+    ofSetColor(255, 0, 0);
+    ofDrawBitmapString("tap here", position.x + size / 5, position.y + size / 2);
+}
+
+
+void ofxBPMSender::mouseReleased(int x, int y){
+    if(position.x < x && x < position.x + size){
+        if(position.y < y && y < position.y + size){
             if(tap_count % 2 == 0){
-                cout << "first_tap" << endl;
+                ofLogNotice() << "first_tap";
                 first_time = ofGetElapsedTimef();
             }
             if(tap_count % 2 == 1){
-                cout << "second_tap" << endl;
+                ofLogNotice() << "second_tap";
                 second_time = ofGetElapsedTimef();
                 second_time_data = second_time;
                 time_distance = second_time - first_time;
@@ -95,18 +78,22 @@ void ofxBPMSender::mouseReleased(ofMouseEventArgs & args){
     }
 }
 
-//--------------------------------------------------------------
-//--------------------------------------------------------------
-//--------------------------------------------------------------
-//--------------------------------------------------------------
-//--------------------------------------------------------------
-void ofxBPMSender::mousePressed(ofMouseEventArgs & args){}
-void ofxBPMSender::mouseMoved(ofMouseEventArgs & args){}
-void ofxBPMSender::mouseDragged(ofMouseEventArgs & args){}
-void ofxBPMSender::mouseScrolled(ofMouseEventArgs & args){}
-void ofxBPMSender::mouseEntered(ofMouseEventArgs & args){}
-void ofxBPMSender::mouseExited(ofMouseEventArgs & args){}
 
 
-void ofxBPMSender::keyPressed(ofKeyEventArgs &args){}
-void ofxBPMSender::keyReleased(ofKeyEventArgs &args){}
+//Private-----------------------------------------------------------
+void ofxBPMSender::calculateBPM(float _time_distance){
+    float _bpm = 60.f / _time_distance;
+    bpm = _bpm;
+}
+
+void ofxBPMSender::noticeBPM(){
+    if(second_time_data + time_distance < ofGetElapsedTimef()){
+        second_time_data += time_distance;
+        isAttack = true;
+    }else{
+        isAttack = false;
+    }
+}
+
+
+
--- src/ofxBPMSender.hpp
+++ src/ofxBPMSender.hpp
@@ -1,61 +1,59 @@
 //
 //  ofxBPMSender.hpp
-//  bpm_sender
+//  ofxBPMSender
 //
-//  Created by kyo imai on 2020/04/06.
+//  Created by 今井亨 on 2020/04/18.
 //
+//This app returns bool type at the tempo you tapped at the time of VJ. The getAttack() type bool is used to receive the tempo, and the getBPM() type float is used to receive the bpm.
+//このappはVJの時にタップしたテンポでbool型を返します。テンポの受け取りにはbool型の getAttack()、bpmの受け取りにはfloat 型getBPM()を使用。
+
+//Basically, we're assuming that you'll be typing in KEY.
+//基本的に、keyでの入力を想定しています。
 
 #pragma once
 
 #include "ofMain.h"
 
 class ofxBPMSender{
-  
-public:
-    ofxBPMSender();
-    ~ofxBPMSender();
     
     
-    static ofEvent<bool> attack;
     
-    float bpm;
-    
-    void setButton(ofVec2f position, ofVec2f size);
-    void updateBPM();
-    void drawButton();
-    
-    float getBPM();
-    void mousePressed(ofMouseEventArgs & args);
-    
-    bool is_on = false; //マウスを押した時
-    bool is_attack = false; //bpm通知
-    
-    
-//
-    void keyPressed(ofKeyEventArgs & args);
-    void keyReleased(ofKeyEventArgs & args);
-    
-//
-    void mouseMoved(ofMouseEventArgs & args);
-    void mouseDragged(ofMouseEventArgs & args);
-    void mouseReleased(ofMouseEventArgs & args);
-    void mouseScrolled(ofMouseEventArgs & args);
-    void mouseEntered(ofMouseEventArgs & args);
-    void mouseExited(ofMouseEventArgs & args);
-    
-private:
-    ofVec2f position;
-    ofVec2f size;
+//  key------------------------
 
-    int tap_count;
     
+//  mouse----------------------
+    glm::vec2 position;
+    float size;
+    
+//    tap_count;
+    int tap_count = 0;
+    
+//    time manager
     float first_time;
     float second_time;
-    float time_distance;
     float second_time_data;
-
+    float time_distance;
     
-    void calculateBPM(float time_distance);
+    void calculateBPM(float _time_distance);
     void noticeBPM();
+    
+    float bpm; // -> getBPM()
+    bool isAttack; // -> getAttack()
+    
+    
+
+public:
+//    絶対に書くやつ
+    void update();
+    float getBPM();
+    bool getAttack();
+    
+//  key入力の時
+    void keyReleased(int key);
+    
+//  mouse入力の時
+    void setupButton(glm::vec2 _position, float _size);
+    void drawButton();
+    void mouseReleased(int x, int y);
 };
 
